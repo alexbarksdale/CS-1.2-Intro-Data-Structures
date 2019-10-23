3840870c127f91481994b25f4969cf9c2f79cce8
alexbarksdale <ale.barksdale@gmail.com>
2019-10-23 11:05:03 -0700

    Added another version for load_words()

--- Code/dictionary_words.py
+++ Code/dictionary_words.py
@@ -1,9 +1,11 @@
 from random import choice
 from sys import argv
+from utils import time_it
 
 
+@time_it
 def load_words(amount):
-    # ? ------- [VESRION 1] -------
+    # ? ------- [VESRION 1 (FASTER)] -------
     # with open('/usr/share/dict/words', 'r') as file:
 
     #     word = file.read().split()
@@ -14,15 +16,23 @@ def load_words(amount):
 
     #     print(' '.join(random_word) + '.')
 
-    # ? ------- [VESRION 2] -------
-    random_word = []
-    for x in range(int(amount)):
-        random_word.append(
-            choice(open('/usr/share/dict/words', 'r').read().split()))
+    # ? ------- [VESRION 2 (SLOWER)] -------
+    # random_word = []
+    # for x in range(int(amount)):
+    #     random_word.append(
+    #         choice(open('/usr/share/dict/words', 'r').read().split()))
 
-    print(' '.join(random_word) + '.')
+    # print(' '.join(random_word) + '.')
+
+    # ? ------- [VESRION 3 (CLOSE TO V1)] -------
+
+    with open('/usr/share/dict/words', 'r') as file:
+        words = [word for word in file.read().split()]
+        random_word = ' '.join(choice(words) for i in range(int(amount))) + '.'
+
+        print(random_word)
 
 
-# Slices from the first(int) argument until the end(':')
+    # Slices from the first(int) argument until the end(':')
 args = argv[1:]
 load_words(args[0])
