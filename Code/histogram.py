0b65ef15d1067dcbe07f60f1b4bf9efac3ad19c9
alexbarksdale <ale.barksdale@gmail.com>
2019-10-29 14:37:02 -0700

    [Add] Sorting algorithm

--- Code/histogram.py
+++ Code/histogram.py
@@ -1,5 +1,3 @@
-from sys import argv
-
 ''' Returns: a histogram of unique words with the number of times the word appears '''
 
 
@@ -17,11 +15,30 @@ def histogram(source_text):
                 word_freq[word] = 1
         return word_freq
 
-        # ? ------- [Easy way] -------
-        # word_freq = [word.count(x) for x in word]
+        # ? ------- [Easy way (SLOW)] -------
+        # word_freq = [source_file.count(word) for word in source_file]
+        # # print(len(word_freq), len(source_file))
 
         # # zip returns a zip object which is an iterator of tuples which pairs the words together
-        # return dict(list(zip(word, word_freq)))
+        # return dict(zip(source_file, word_freq))
+
+
+'''
+Sorts the word frequency.
+'Lambda' is used to define anonymous functions which are functions with no name.
+Acknowledgement: https://www.saltycrane.com/blog/2007/09/how-to-sort-python-dictionary-by-keys/
+'''
+
+
+def sort_histo(histo):
+    for key, value in sorted(histo.items(), reverse=False, key=lambda item: item[1]):
+        print(f'{key} - {value}')
+
+# def sort_histo(histo):
+#     sort_freq = [(histo[key], key) for key in histo]
+#     sort_freq.sort()
+#     sort_freq.reverse()
+#     return sort_freq
 
 
 ''' Returns: the total count of words in histogram() '''
@@ -42,6 +59,5 @@ if __name__ == '__main__':
     source = '/Users/alexbarksdale/Dropbox/[DEV]Development/Make School/CS-1.2-Intro-Data-Structures/Code/tweetgen.txt'
 
     histo = histogram(source)
-    print(histo)
-
-    print(unique_words(histo))
+    # print(histo)
+    sort_histo(histo)
