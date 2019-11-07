05718be40094b43c469ff9926b311a0bdc0dabe2
alexbarksdale <ale.barksdale@gmail.com>
2019-11-06 16:01:32 -0800

    [Add] sample function

--- Code/dictogram.py
+++ Code/dictogram.py
@@ -1,7 +1,7 @@
 #!python
 
 from __future__ import division, print_function  # Python 2 and 3 compatibility
-import random
+from random import randint
 
 
 class Dictogram(dict):
@@ -25,6 +25,7 @@ class Dictogram(dict):
             self[word] += count
         else:
             self[word] = count
+
         self.types = len(set(self))
         self.tokens = sum(self[word] for word in self)
 
@@ -42,6 +43,15 @@ class Dictogram(dict):
         """Return a word from this histogram, randomly sampled by weighting
         each word's probability of being chosen by its observed frequency."""
         # TODO: Randomly choose a word based on its frequency in this histogram
+        hist_length = sum(self.values())
+        word_predict = randint(1, hist_length)
+        counter = 0
+
+        for word in self.keys():
+            counter += self[word]
+
+            if word_predict <= counter:
+                return word
 
 
 def print_histogram(word_list):
@@ -56,44 +66,44 @@ def print_histogram(word_list):
         freq = histogram.frequency(word)
         print('{!r} occurs {} times'.format(word, freq))
     print()
-    # print_histogram_samples(histogram)
+    print_histogram_samples(histogram)
 
 
-# def print_histogram_samples(histogram):
-#     print('Histogram samples:')
-#     # Sample the histogram 10,000 times and count frequency of results
-#     samples_list = [histogram.sample() for _ in range(10000)]
-#     samples_hist = Dictogram(samples_list)
-#     print('samples: {}'.format(samples_hist))
-#     print()
-#     print('Sampled frequency and error from observed frequency:')
-#     header = '| word type | observed freq | sampled freq  |  error  |'
-#     divider = '-' * len(header)
-#     print(divider)
-#     print(header)
-#     print(divider)
-#     # Colors for error
-#     green = '\033[32m'
-#     yellow = '\033[33m'
-#     red = '\033[31m'
-#     reset = '\033[m'
-#     # Check each word in original histogram
-#     for word, count in histogram.items():
-#         # Calculate word's observed frequency
-#         observed_freq = count / histogram.tokens
-#         # Calculate word's sampled frequency
-#         samples = samples_hist.frequency(word)
-#         sampled_freq = samples / samples_hist.tokens
-#         # Calculate error between word's sampled and observed frequency
-#         error = (sampled_freq - observed_freq) / observed_freq
-#         color = green if abs(error) < 0.05 else yellow if abs(
-#             error) < 0.1 else red
-#         print('| {!r:<9} '.format(word)
-#               + '| {:>4} = {:>6.2%} '.format(count, observed_freq)
-#               + '| {:>4} = {:>6.2%} '.format(samples, sampled_freq)
-#               + '| {}{:>+7.2%}{} |'.format(color, error, reset))
-#     print(divider)
-#     print()
+def print_histogram_samples(histogram):
+    print('Histogram samples:')
+    # Sample the histogram 10,000 times and count frequency of results
+    samples_list = [histogram.sample() for _ in range(10000)]
+    samples_hist = Dictogram(samples_list)
+    print('samples: {}'.format(samples_hist))
+    print()
+    print('Sampled frequency and error from observed frequency:')
+    header = '| word type | observed freq | sampled freq  |  error  |'
+    divider = '-' * len(header)
+    print(divider)
+    print(header)
+    print(divider)
+    # Colors for error
+    green = '\033[32m'
+    yellow = '\033[33m'
+    red = '\033[31m'
+    reset = '\033[m'
+    # Check each word in original histogram
+    for word, count in histogram.items():
+        # Calculate word's observed frequency
+        observed_freq = count / histogram.tokens
+        # Calculate word's sampled frequency
+        samples = samples_hist.frequency(word)
+        sampled_freq = samples / samples_hist.tokens
+        # Calculate error between word's sampled and observed frequency
+        error = (sampled_freq - observed_freq) / observed_freq
+        color = green if abs(error) < 0.05 else yellow if abs(
+            error) < 0.1 else red
+        print('| {!r:<9} '.format(word)
+              + '| {:>4} = {:>6.2%} '.format(count, observed_freq)
+              + '| {:>4} = {:>6.2%} '.format(samples, sampled_freq)
+              + '| {}{:>+7.2%}{} |'.format(color, error, reset))
+    print(divider)
+    print()
 
 
 def main():
