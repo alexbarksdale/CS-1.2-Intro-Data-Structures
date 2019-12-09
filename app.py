efb66048830e9e9555b778242bd13e064e3d29ca
alexbarksdale <ale.barksdale@gmail.com>
2019-12-08 22:35:06 -0800

    [Add] Generating quotes on the site

--- app.py
+++ app.py
@@ -5,13 +5,27 @@ import os
 
 
 app = Flask(__name__)
+# Stores the markov chain
+markov = Markov('Code/tweetgen.txt')
 
 
 @app.route('/')
 def index():
-    ngram_markov = Markov('Code/tweetgen.txt').n_markov
-    return render_template('index.html', ngram_markov=ngram_markov)
+    return render_template('base.html', markov=markov)
 
+
+@app.route('/generateQuote')
+def generate():
+    return markov.gen_ngram_markov()
+
+
+if app.config["DEBUG"]:
+    @app.after_request
+    def after_request(response):
+        response.headers["Cache-Control"] = "no-cache, no-store, must-revalidate, public, max-age=0"
+        response.headers["Expires"] = 0
+        response.headers["Pragma"] = "no-cache"
+        return response
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0', port=os.environ.get('PORT', 5000))
